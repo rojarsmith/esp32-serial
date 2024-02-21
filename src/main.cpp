b414598304c6f9a64e2126d64097025b50535ab6
Rojar Smith <rojarsmith@gmail.com>
2024-02-21 08:22:37 +0800

    feat: Initial UART2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
+/*
+Board: Nodemcu-32S
+*/
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define __DEBUG__
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#ifdef __DEBUG__
+#define DEBUG(...)                 \
+  Serial.println(__VA_ARGS__);     \
+  Serial.print(" @ [SRC]:      "); \
+  Serial.println(__FILE__);        \
+  Serial.print(" @ [LINE]:     "); \
+  Serial.println(__LINE__);        \
+  Serial.print(" @ [FUNCTION]: "); \
+  Serial.println(__func__);
+#else
+#define DEBUG(...)
+#endif
+
+#define RXD2 16
+#define TXD2 17
+
+#define rxSIZES (4 * 1024)
+
+char dbgBuf[rxSIZES + 1];
+
+void setup()
+{
+  Serial.begin(115200);
+  DEBUG("Setup ESP32 Start.");
+  DEBUG("Enable DEBUG mode.");
+
+  Serial2.begin(115200 * 1, SERIAL_8N1, RXD2, TXD2);
+  sprintf(dbgBuf, "Serial2:%d, %d, %d, %d", 115200 * 1, SERIAL_8N1, RXD2, TXD2);
+  DEBUG(dbgBuf);
+  Serial2.println(dbgBuf);
+
+  DEBUG("Setup ESP32 completed.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
 }
